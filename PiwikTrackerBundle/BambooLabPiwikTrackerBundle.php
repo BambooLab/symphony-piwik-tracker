60b16ccc3e39f6da445ab498ad571581205e934d
Ярослав <yaroslav.help@gmail.com>
2015-07-07 19:51:39 +0300

    init

---
+++ PiwikTrackerBundle/BambooLabPiwikTrackerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BambooLab\PiwikTracker\PiwikTrackerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BambooLabPiwikTrackerBundle extends Bundle
+{
+}
---
+++ PiwikTrackerBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BambooLab\PiwikTracker\PiwikTrackerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('BambooLabPiwikTrackerBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
